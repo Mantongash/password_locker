ecda00c0acfddfaf37b905c75b77a9a08c089049
Mantongash <you@example.com>
2019-12-15 14:29:32 +0300

    Declared variables for username and password under the init method

--- locker.py
+++ locker.py
@@ -4,7 +4,9 @@ class Users:
     """
     user_list = []  # Blank user list to store user details
 
-    def __init__(self,username, password):
+    def __init__(self, username, password):
         """
         Defining the init method (constructor) that will be used to create new user instances
         """
+        self.username = username
+        self.password = password
