f506d05f2903b329cb0da3341518b22833726af1
Mantongash <you@example.com>
2019-12-15 13:58:09 +0300

    Added BDD as a comment for refference

---
+++ test_locker.py
@@ -0,0 +1,8 @@
+############### BDD #######################
+# 1. Create Password Locker Account.
+# 2. Store account credentials.
+# 3. Create new password credentials.
+# 4. Input my own password.
+# 5. View saved account credentials.
+# 5. Delete an account credential.
+##########################################
