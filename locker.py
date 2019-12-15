0dccc0ab84f4e9bd79c6798d434c294fb7d43b7b
Mantongash <you@example.com>
2019-12-15 14:28:07 +0300

    Add docstring to the init method. Also included the self parameter that I had forgotten

--- locker.py
+++ locker.py
@@ -2,6 +2,9 @@ class Users:
     """
     A class to store user log in details.
     """
-    user_list = [] #Blank user list to store user details
+    user_list = []  # Blank user list to store user details
 
-    def __init__ (username, password):
+    def __init__(self,username, password):
+        """
+        Defining the init method (constructor) that will be used to create new user instances
+        """
