a6fe359b7b952acb5486a417738ff5b1bf27081b
Mantongash <you@example.com>
2019-12-16 16:31:39 +0300

    Create logic for adding users

--- run_locker.py
+++ run_locker.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3.6
 from locker import Users, Credentials
+from pynput.keyboard import Key, Controller
+from termcolor import colored
+import time
 # import pyperclip
 
 # Users
@@ -12,11 +15,13 @@ def create_user(uname, password):
     new_user = Users(uname, password)
     return new_user
 
+
 def save_user(user):
     '''
     Function to save a user
     '''
     user.save_user()
+
 
 def del_cont(user):
     '''
@@ -24,8 +29,8 @@ def del_cont(user):
     '''
     user.delete_contact()
 
-
     # Credentials
+
 
 def create_credential(uname, account, account_username, account_password):
     '''
@@ -57,80 +62,93 @@ def display_credentials():
 
 
 def main():
-    print("Hello Welcome to your contact list. What is your name?")
+    # keyboard = Controller()
+    # time.sleep(2)
+    print("Hello, WELCOME to THE PASSWORD LOCKER. What is your name, please :) ?")
     user_name = input()
 
-    print(f"Hello {user_name}. what would you like to do?")
+    print(f"Hello {user_name}. what would you like to do today?")
     print('\n')
 
     while True:
-        print("Use these short codes : cc - create a new contact, dc - display contacts, fc -find a contact, ex -exit the contact list ")
+        print("Use these short codes : ca - create a new account,  li - log in, dc - display credentials, cc - create credential - ex - exit the password locker")
 
         short_code = input().lower()
 
-        if short_code == 'cc':
-            print("New Contact")
-            print("-"*10)
-
-            print("First name ....")
-            f_name = input()
-
-            print("Last name ...")
-            l_name = input()
-
-            print("Phone number ...")
-            p_number = input()
-
-            print("Email address ...")
-            e_address = input()
-
-            # create and save new contact.
-            save_contacts(create_contact(
-                f_name, l_name, p_number, e_address))
+        if short_code == 'ca':
+            print(
+                "Awesome choice: Kindly provide me with a username and a password")
             print('\n')
-            print(f"New Contact {f_name} {l_name} created")
+            print("*"*100)
             print('\n')
 
-        elif short_code == 'dc':
+            print("Username:")
+            uname = input()
 
-            if display_contacts():
-                print("Here is a list of all your contacts")
-                print('\n')
+            print("Password:")
+            password = input()
 
-                for contact in display_contacts():
-                    print(
-                        f"{contact.first_name} {contact.last_name} .....{contact.phone_number}")
+           # create and save new user.
+            save_user(create_user(
+                uname, password))
+            print('\n')
+            print(
+                f"Account Details: Username ** {uname} ** \nPassword ** {password} **")
+            print('\n')
 
-                print('\n')
-            else:
-                print('\n')
-                print("You dont seem to have any contacts saved yet")
-                print('\n')
+        elif short_code == "li":
+            print("Kindly enter your details to log-in")
+            print("Username:")
+            username = input()
 
-        elif short_code == 'fc':
+            print("Password:")
+            password = input()
+            for user in Users.user_list:
+                if user == username:
+                    print("User is already registered")
+                else:
+                    print("You are logged in")
+        # elif short_code ==
 
-            print("Enter the number you want to search for")
+        # elif short_code == 'dc':
 
-            search_number = input()
-            if check_existing_contacts(search_number):
-                search_contact = find_contact(search_number)
-                print(
-                    f"{search_contact.first_name} {search_contact.last_name}")
-                print('-' * 20)
+            #     if display_contacts():
+            #         print("Here is a list of all your contacts")
+            #         print('\n')
 
-                print(
-                    f"Phone number.......{search_contact.phone_number}")
-                print(f"Email address.......{search_contact.email}")
-            else:
-                print("That contact does not exist")
+            #         for contact in display_contacts():
+            #             print(
+            #                 f"{contact.first_name} {contact.last_name} .....{contact.phone_number}")
 
-        elif short_code == "ex":
-            print("Bye .......")
-            break
-        else:
-            print("I really didn't get that. Please use the short codes")
+            #         print('\n')
+            #     else:
+            #         print('\n')
+            #         print("You dont seem to have any contacts saved yet")
+            #         print('\n')
+
+            # elif short_code == 'fc':
+
+            #     print("Enter the number you want to search for")
+
+            #     search_number = input()
+            #     if check_existing_contacts(search_number):
+            #         search_contact = find_contact(search_number)
+            #         print(
+            #             f"{search_contact.first_name} {search_contact.last_name}")
+            #         print('-' * 20)
+
+            #         print(
+            #             f"Phone number.......{search_contact.phone_number}")
+            #         print(f"Email address.......{search_contact.email}")
+            #     else:
+            #         print("That contact does not exist")
+
+            # elif short_code == "ex":
+            #     print("Bye .......")
+            #     break
+            # else:
+            #     print("I realsly didn't get that. Please use the short codes")
 
 
 if __name__ == '__main__':
-
     main()
